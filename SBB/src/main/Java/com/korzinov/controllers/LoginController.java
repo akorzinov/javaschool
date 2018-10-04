9ecdece911e4b11d38f191c551af50701fc30878
Aleksandr <aleksandrkorzinov@gmail.com>
2018-10-05 01:01:08 +0300

    first commit

---
+++ SBB/src/main/Java/com/korzinov/controllers/LoginController.java
@@ -0,0 +1,63 @@
+package com.korzinov.controllers;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean (name="LoginController")
+@SessionScoped
+public class LoginController {
+    private String login;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String checkLogin(){
+        return "welcome?faces-redirect=true";
+    }
+
+    public String registration() {
+        return "registration?faces-redirect=true";
+    }
+
+}
